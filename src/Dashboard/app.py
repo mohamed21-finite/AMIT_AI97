0febe8561586a6b00f5ed01272f598fae62e0283
mohamed21-finite <mohamed.ali006@eng-st.cu.edu.eg>
2026-05-20 19:36:13 +0300

    Dashboard

---
+++ src/Dashboard/app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from dash import Dash, html, dcc
+import plotly.express as px
+
+
+df = pd.read_csv('city_sustainability_geospatial.csv')
+
+app = Dash(__name__)
+
+population_vs_co2 = px.scatter(df, x="population_millions", y="co2_tons_per_capita", color='continent', size='avg_temp_c', hover_name='city',
+hover_data=['renewable_energy_share', 'air_quality_index'])
+
+pop_hist = px.histogram(df, x='population_millions', nbins=40, color='continent', marginal='box')
+
+
+app.title = "City Sustainability Dashboard"
+app.layout = html.Div([
+    html.H1("Renewable Energy and CO2 Emissions in Global Cities"),
+    dcc.Graph(figure=population_vs_co2),
+    dcc.Graph(figure=pop_hist)
+])
+
+app.run()
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+
 pairs = [(1, 2), (3, 4), (5, 6)]
 
 for a , b in pairs:
